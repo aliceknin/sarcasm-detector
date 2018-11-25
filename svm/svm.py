added scaling, fixed sentiment

svm/svm.py
@@ -6,8 +6,8 @@ from sklearn.metrics import mean_squared_error
 from scipy.stats import pearsonr
 
 def train_and_validate_svm(train_data, train_labels, test_data, test_labels, \
-                           kernel='linear', C=1.0, verbose=False):
-    svm = SVC(kernel=kernel, C=C, verbose=verbose)
+                           kernel='rbf', C=1.0, gamma='scale', verbose=False):
+    svm = SVC(kernel=kernel, C=C, gamma='scale', verbose=verbose)
     svm.fit(train_data, train_labels)
     predicted_labels = svm.predict(test_data)
     mse = mean_squared_error(test_labels, predicted_labels)
@@ -56,24 +56,3 @@ def print_metric_results_of_trials(metrics_over_trials, metric_name):
     print("%s mean: %7.4f" % (metric_name, avg_metric))
     print("%s max:  %7.4f" % (metric_name, max_metric))
     print(80*'=')
-
-# ------------------------------------------------------------------------
-# Tests ---
-# ------------------------------------------------------------------------
-
-# if __name__ == '__main__':
-#
-#     embeddings = utils.load_word2vec_embeddings()
-#
-#     summaries, nr, fluencies = utils.get_summaries_and_scores()
-#     summary_words = utils.get_summary_words(summaries)
-#
-#     non_redundancy_features = utils.non_redundancy_input(summary_words, embeddings)
-#     print("non redundancy features:", non_redundancy_features[:5])
-#
-#     kernel = 'linear'
-#     C = 1.0
-#     verbose = True
-#
-#     mses, pearsons = \
-#         svm.cross_validate_svm(non_redundancy_features, nr, 10, kernel, C, verbose)

svm/utils.py
@@ -3,7 +3,9 @@ import nltk
 import math
 import numpy as np
 from nltk.corpus import stopwords
+from nltk.corpus import sentiwordnet
 import svm
+from sklearn import preprocessing
 
 from datetime import datetime
 
@@ -250,6 +252,8 @@ def get_sentiments_tweets(tweets):
         if i+1 % 1000 == 0:
             print()
     print()
+    fdist = nltk.FreqDist(scores)
+    print('most common 50 sentiments:', fdist.most_common(50))
     return scores
 
 ############################## Assemble Features ##############################
@@ -327,11 +331,14 @@ if __name__ == '__main__':
     np_train_labels = np.array(_train_labels)
     np_test_labels = np.array(_test_labels)
 
-    print(np_train_features[:2])
-
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")
 
-    svm.cross_validate_svm(np_train_features, np_train_labels, kernel="rbf", verbose=True)
+    scaled_train_features = preprocessing.scale(np_train_features)
+    scaled_test_features = preprocessing.scale(np_test_features)
+
+    print(scaled_train_features[:10])
+
+    svm.cross_validate_svm(scaled_train_features, np_train_labels, kernel="rbf")
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")